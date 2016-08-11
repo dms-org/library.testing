a3aaa5fc09e8b37d034dd01f2cd49aae764b8e33
Elliot Levin <elliot@iddigital.com.au>
2016-08-11 14:52:32 +1000

    Add entity persistence test case

---
+++ src/TestCase/EntityPersistenceTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Dms\Library\Testing\TestCase;
+
+use Dms\Core\Persistence\DbRepository;
+use Dms\Core\Tests\Persistence\Db\Integration\Mapping\DbIntegrationTest;
+
+/**
+ * @author Elliot Levin <elliotlevin@hotmail.com>
+ */
+abstract class EntityPersistenceTest extends DbIntegrationTest
+{
+    /**
+     * @return string
+     */
+    abstract protected function getEntityClass() : string;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->orm->enableLazyLoading(false);
+
+        $this->repo = new DbRepository($this->connection, $this->orm->getEntityMapper($this->getEntityClass()));
+    }
+}
